c5a6532798c1641de1e97a747528d6e032562b8f
MaePloyKitchen <mcgregor.ryan.1999@gmail.com>
2022-08-19 20:41:55 -0400

    Bruh

--- animesuggestor.py
+++ animesuggestor.py
@@ -5,7 +5,7 @@ import json
 # import time
 
 
-def link_build(genre, page):
+def link_build_basic(genre, page):
 
     id_genre_mapping = {
         "Action": 1,
@@ -38,10 +38,13 @@ def link_build(genre, page):
     # return contain
     return link
 
+def link_build_advanced(url,token):
+    new_link = url + f"\{token}"
+    return new_link
 
 def get_anime_basic(genre, page):
     results = []
-    webpage = requests.get(link_build(genre, page))
+    webpage = requests.get(link_build_basic(genre, page))
     soup = BeautifulSoup(webpage.content, "html.parser")
     containers = soup.find_all(class_="js-anime-category-producer")
 
@@ -61,13 +64,45 @@ def get_anime_basic(genre, page):
 
 
 def get_anime_advanced(url):
-    results = []
     webpage = requests.get(url)
     soup = BeautifulSoup(webpage.content, "html.parser")
+    data = [spaceit.text for spaceit in soup.find(class_="borderClass").find_all(class_="spaceit_pad")]
+    cleaned_up_data = [point.replace("\n","").split(":")[1].strip() for point in data]
+    cleaned_up_columns = [point.replace("\n","").split(":")[0] for point in data]
+    mapped_data = dict(zip(cleaned_up_columns,cleaned_up_data))
+    mapped_data["Score"] = mapped_data["Score"].split(" ")[0]
+    mapped_data["Popularity"] = mapped_data["Popularity"].replace("#","")
+    mapped_data["Ranked"] = mapped_data["Ranked"].split("22 ")[0].replace("#","")
 
+    if "Theme" in mapped_data:
+            mapped_data["Theme"] = ",".join([theme[:len(theme)//2] for theme in mapped_data["Theme"].replace(" ","").split(",")])
+    elif "Themes" in mapped_data:
+        mapped_data["Themes"] = ",".join([theme[:len(theme)//2] for theme in mapped_data["Themes"].replace(" ","").split(",")])
+    
+    if "Genres" in mapped_data:
+        mapped_data["Genres"] = ",".join([genre[:len(genre)//2] for genre in mapped_data["Genres"].replace(" ","").split(",")])
+    elif "Genre" in mapped_data:
+        mapped_data["Genre"] = ",".join([genre[:len(genre)//2] for genre in mapped_data["Genre"].replace(" ","").split(",")])
 
-genre = "Adventure"
-page = 1
+    if "Demographic" in mapped_data:
+        mapped_data["Demographic"] = ",".join([demo[:len(demo)//2] for demo in mapped_data["Demographic"].replace(" ","").split(",")])
+    
+    mapped_data["Producers"] = ",".join([producer.strip() for producer in mapped_data["Producers"].split(",")])
+    if not len(mapped_data["Aired"].split("to")) < 2:
+        air_start = mapped_data["Aired"].split("to")[0].strip()
+        air_finish = mapped_data["Aired"].split("to")[1].strip()
+        mapped_data.pop("Aired")
+        mapped_data["Air_Start"] = air_start
+        mapped_data["Air_Finish"] = air_finish
+    return mapped_data
 
-with open(f"animedump_{genre}.json", "w") as infile:
-    json.dump(get_anime_basic(genre, 1), infile)
+def get_anime_stats(url):
+    link = link_build_advanced(url,"stats")
+    webpage = requests.get(link)
+    soup = BeautifulSoup(webpage.content, "html.parser")
+    
+
+    
+
+def get_anime_characters(url):
+    link_build_advanced(url,"characters")
